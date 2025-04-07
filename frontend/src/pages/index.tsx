446ff7d979ab64ff39aabf7b2ae3cfd556349f0e
Keith Frederick <keithfrederick@Keiths-iMac.local>
2025-04-06 23:43:25 -0500

    Add frontend changes

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/ui/components/Navbar";
+import Hero from "../components/ui/components/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
